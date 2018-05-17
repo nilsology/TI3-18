c861de11f59cbafed990169460edeb26a7a9a45f
Nils Koppelmann <koppelmann@andorra.imp.fu-berlin.de>
2018-05-18 01:21:14 +0200

    add working my_cat.c

---
+++ ue03/my_cat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFF_SIZE 2048
+int main(int argc, char const *argv[])
+{
+  FILE* file;
+  char buffer[BUFF_SIZE];
+
+  if (argc < 2)
+  {
+    // read from stdin
+    file = stdin;
+  } else if (argc >= 2)
+  {
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+      fprintf(stderr, "Error opening %s.\n", argv[1]);
+      return 1;
+    }
+  }
+
+  while (fgets(buffer, BUFF_SIZE, file) != NULL)
+    fprintf(stdout, "%s", buffer);
+
+  fclose(file);
+  return 0;
+}
+
